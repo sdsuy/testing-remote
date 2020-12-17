f4db7ec1e43ae586dd31b70eaf10b687592ae5cf
Santiago Da Silva <sdasilva@outlook.es>
2020-12-16 23:40:37 -0300

    first commit

---
+++ src/main/java/io/spring/testingremote/TestingRemoteApplication.java
@@ -0,0 +1,13 @@
+package io.spring.testingremote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingRemoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingRemoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/spring/testingremote/TestingRemoteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.testingremote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingRemoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
