ab876f89c35906e947395252b3a028ac9b32b825
Santiago Da Silva <sdasilva@outlook.es>
2020-12-17 03:24:54 -0300

    Testing Title and Heading page using Selenium WebDriver, ChromeDriver and Hamcrest

---
+++ src/test/java/io/spring/testingremote/TestingConsoleApptimTests.java
@@ -0,0 +1,58 @@
+package io.spring.testingremote;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.text.IsEqualIgnoringCase.equalToIgnoringCase;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+class TestingConsoleApptimTests {
+	
+	@Autowired
+	WebDriver driver;
+	
+	@BeforeEach
+	void setUp() throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Spring\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://console.apptim.com");
+		Thread.sleep(3000);  // Let the user actually see something!
+	    
+	}
+
+	@Test
+	void loginTitleTest() {
+
+	    String actualTitle = driver.getTitle();
+	    String expectedTitle = "Apptim - Log in";
+	    assertThat(actualTitle, equalToIgnoringCase(expectedTitle));
+		
+	}
+	
+	@Test
+	void signInHeadingTest() {
+
+	    String actualHeading = driver.findElement(By.cssSelector("h2")).getText();
+	    String expectedHeading = "Sign in to continue";
+	    assertThat(expectedHeading, equalToIgnoringCase(actualHeading));
+		
+	}
+	
+	@AfterEach
+	void closeUp() {
+		
+		driver.quit();
+		
+	}
+
+}
